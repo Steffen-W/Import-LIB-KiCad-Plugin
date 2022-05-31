5faaec75c033e8bdafb4f57e2c953d0caf766e3b
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2022-05-31 19:56:30 +0900

    snapeda 3d model import working but bugs with ultralib and rotation

--- impart.py
+++ impart.py
@@ -4,8 +4,9 @@
 # Assembles local KiCad component libraries from downloaded Octopart,
 # Samacsys, Ultralibrarian and Snapeda zipfiles. Currently assembles just
 # symbols and footptints. Tested with KiCad 5.1.12 for Ubuntu.
+import shutil
 
-from mydirs import SRC_PATH, LIB_PATH  # *CONFIGURE ME*
+from mydirs import SRC_PATH, LIB_PATH, REMOTE_3DMODEL_DIR  # *CONFIGURE ME*
 import argparse
 import re
 import readline
@@ -95,6 +96,8 @@ def import_all(zip_file):
             dcm_path = root / 'device_name.dcm'
             lib_path = root / 'device_name.lib'
             footprint_dir_path = root / 'device_name.pretty'
+            model_path = root / 'device_name.step'
+            # todo fill in model path for OCTOPART
             if dcm_path.exists() and lib_path.exists() and footprint_dir_path.exists():
                 remote_type = 0  # OCTOPART
                 break
@@ -104,6 +107,7 @@ def import_all(zip_file):
                 dcm_path = unzip(directory, '.dcm')
                 lib_path = unzip(directory, '.lib')
                 footprint_dir_path = directory
+                # todo fill in model path for SAMACSYS
                 assert dcm_path and lib_path, 'Not in samacsys format'
                 remote_type = 1  # SAMACSYS
                 break
@@ -113,6 +117,7 @@ def import_all(zip_file):
                 dcm_path = unzip(directory, '.dcm')
                 lib_path = unzip(directory, '.lib')
                 footprint_dir_path = unzip(directory, '.pretty')
+                model_path = unzip(root, '.step')
                 assert lib_path and footprint_dir_path, 'Not in ultralibrarian format'
                 remote_type = 2  # ULTRALIBRARIAN
                 break
@@ -121,6 +126,7 @@ def import_all(zip_file):
             if lib_path:
                 dcm_path = unzip(root, '.dcm')
                 footprint_dir_path = root
+                model_path = root
                 remote_type = 3  # SNAPEDA
                 break
 
@@ -203,9 +209,8 @@ def import_all(zip_file):
                     elif line.startswith('$CMP '):
                         component_name = line[5:].strip()
                         if component_name.startswith(device_name):
-                            yes = input(device_name + ' in ' + dcm_file_read.name + ', replace it? [Yes]: ') or "Yes"
-                            overwrite_existing = yes and 'yes'.startswith(yes.lower())  # todo should also accept y or Y
-                            if not overwrite_existing:
+                            overwrite_existing = input(device_name + ' in ' + dcm_file_read.name + ', replace it? [Yes]: ') or "Yes"
+                            if overwrite_existing not in ('y', 'yes', 'Yes', 'Y', 'YES'):
                                 return 'OK:', device_name, 'already in', dcm_file_read.name
                             writefile.write('\n'.join(dcm_attributes[index_start:index_end]) + '\n')
                             overwrote_existing = True
@@ -218,6 +223,23 @@ def import_all(zip_file):
                         writefile.write(line)
 
         # --------------------------------------------------------------------------------------------------------
+        # 3D Model file extraction
+        # --------------------------------------------------------------------------------------------------------
+        found_model = None
+        if not REMOTE_3DMODEL_DIR.is_dir():
+            REMOTE_3DMODEL_DIR.mkdir(parents=True)
+
+        for model_dir_item in model_path.iterdir():
+            if model_dir_item.name.endswith('.step'):
+                if (REMOTE_3DMODEL_DIR / model_dir_item.name).exists():
+                    overwrite_existing = input("Model already exists. Overwrite existing model? [Yes]: ") or "Yes"
+                    if overwrite_existing not in ('y', 'yes', 'Yes', 'Y', 'YES'):
+                        return 'OK:', model_dir_item, 'already in', str(model_path)
+
+                zf.extract(model_dir_item.name, REMOTE_3DMODEL_DIR)
+                found_model = model_dir_item
+
+        # --------------------------------------------------------------------------------------------------------
         # Footprint file parsing
         # todo it doesn't look like this handles duplicates like the other parsing sections
         # --------------------------------------------------------------------------------------------------------
@@ -225,16 +247,57 @@ def import_all(zip_file):
         for footprint_dir_item in footprint_dir_path.iterdir():
             if footprint_dir_item.name.endswith('.kicad_mod') or footprint_dir_item.name.endswith('.mod'):
                 pretty += 1
-                footprint_lines = footprint_dir_item.read_text()
+                footprint = footprint_dir_item.read_text()
 
-                if not (LIB_PATH / (REMOTE_TYPES[remote_type] + '.pretty')).is_dir():
-                    (LIB_PATH / (REMOTE_TYPES[remote_type] + '.pretty')).mkdir(parents=True)
+                footprint_write_path = (LIB_PATH / (REMOTE_TYPES[remote_type] + '.pretty'))
+                if not footprint_write_path.is_dir():
+                    footprint_write_path.mkdir(parents=True)
 
-                if not (LIB_PATH / (REMOTE_TYPES[remote_type] + '.pretty') / footprint_dir_item.name).exists():
-                    (LIB_PATH / (REMOTE_TYPES[remote_type] + '.pretty') / footprint_dir_item.name).touch(mode=0o666)
+                if not (footprint_write_path / footprint_dir_item.name).exists():
+                    (footprint_write_path / footprint_dir_item.name).touch(mode=0o666)
 
-                with (LIB_PATH / (REMOTE_TYPES[remote_type] + '.pretty') / footprint_dir_item.name).open('wt') as wr:
-                    wr.write(footprint_lines)
+                if found_model:
+                    footprint.splitlines()
+                    model = ["  (model \"" + "${REMOTE_3DMODEL_DIR}/" + found_model.name + "\"",
+                             "    (offset (xyz 0 0 0))", "    (scale (xyz 1 1 1))", "    (rotate (xyz 0 0 0))", "  )"]
+
+                    footprint_file_read = footprint_write_path / footprint_dir_item.name
+                    footprint_file_write = footprint_write_path / (footprint_dir_item.name + "~")
+                    overwrite_existing = overwrote_existing = False
+                    if footprint_file_read.exists():
+                        overwrite_existing = input(
+                            "Footprint already exists. Overwrite existing footprint? [Yes]: ") or "Yes"
+                        if overwrite_existing not in ('y', 'yes', 'Yes', 'Y', 'YES'):
+                            return 'OK:', footprint_dir_item.name, 'already in', footprint_file_read.name
+
+                    footprint_file_read.touch(mode=0o666)
+                    with (footprint_write_path / footprint_dir_item.name).open('wt') as wr:
+                        wr.write(footprint)
+                        overwrote_existing = True
+                    if not footprint_file_write.exists():
+                        footprint_file_write.touch(mode=0o666)
+
+                    with footprint_file_read.open('rt') as readfile:
+                        with footprint_file_write.open('wt') as writefile:
+
+                            if stat(footprint_file_read).st_size == 0:
+                                # todo Handle appending to empty file?
+                                pass
+
+                            lines = readfile.readlines()
+
+                            for line_idx, line in enumerate(lines):
+                                if line_idx == len(lines) - 1:
+                                    writefile.writelines(model_line + '\n' for model_line in model)
+                                    writefile.write(line)
+                                    break
+                                else:
+                                    writefile.write(line)
+
+                else:
+                    with (footprint_write_path / footprint_dir_item.name).open('wt') as wr:
+                        wr.write(footprint)
+
         print('footprints:', pretty)
 
         # --------------------------------------------------------------------------------------------------------
@@ -331,7 +394,9 @@ def import_all(zip_file):
                         writefile.write(line)
 
         dcm_file_write.replace(dcm_file_read)
+        footprint_file_write.replace(footprint_file_read)
         lib_file_write.replace(lib_file_read)
+
 
     return 'OK:',
 
--- mydirs.py
+++ mydirs.py
@@ -2,3 +2,4 @@ from pathlib import Path
 
 SRC_PATH = Path.home() / 'Downloads/remotelibs'
 LIB_PATH = Path.home() / 'private/edn/kicad-libs'
+REMOTE_3DMODEL_DIR = LIB_PATH / 'models'
