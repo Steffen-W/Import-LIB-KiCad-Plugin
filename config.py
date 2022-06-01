9bf5f9913978463eaa6457c3d716370f9e581eda
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2022-06-01 18:05:33 +0900

    Replaced config paths with Path objects rather than strings

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
 SRC_PATH = Path.home() / 'Downloads/remotelibs'
-REMOTE_LIB_PATH = "~/kicad-imports/kicad-symbols"
-REMOTE_FOOTPRINTS_PATH = "~/kicad-imports/kicad-footprints"
-REMOTE_3DMODEL_PATH = "~/kicad-imports/kicad-packages3D"
+REMOTE_LIB_PATH = Path.home() / "kicad-imports/kicad-symbols"
+REMOTE_FOOTPRINTS_PATH = Path.home() / "kicad-imports/kicad-footprints"
+REMOTE_3DMODEL_PATH = Path.home() / "kicad-imports/kicad-packages3D"
