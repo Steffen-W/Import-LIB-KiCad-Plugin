291b9a199619892ca15044276b5a7970754b4fcc
Steffen Wittemeier <uni@steffenwittemeier.de>
2023-10-17 16:19:38 +0200

    adjust generate_zip

--- generate_zip.sh
+++ generate_zip.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 rm Import-LIB-KiCad-Plugin.zip
-
-jq --arg today "$(date +%Y-%m-%d)" '.versions[0].version |= $today' metadata.json > metadata.json
+mv metadata.json metadata_.json
+jq --arg today "$(date +%Y-%m-%d)" '.versions[0].version |= $today' metadata_.json > metadata.json
 
 git ls-files  -- 'metadata.json' 'resources*.png' 'plugins*.png' 'plugins*.py' | xargs zip Import-LIB-KiCad-Plugin.zip
+mv metadata_.json metadata.json
