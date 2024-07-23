3ad5a355612462590fb9849b9cabe9b999a4f1f9
Steffen Wittemeier <uni@steffenwittemeier.de>
2024-07-23 08:50:18 +0200

    add easyeda2kicad.py/easyeda2kicad to plugins

--- generate_zip.sh
+++ generate_zip.sh
@@ -6,3 +6,10 @@ jq --arg today "$(date +%Y.%m.%d)" '.versions[0].version |= $today' metadata_.js
 
 git ls-files  -- 'metadata.json' 'resources*.png' 'plugins*.png' 'plugins*.py' | xargs zip Import-LIB-KiCad-Plugin.zip
 mv metadata_.json metadata.json
+
+# add easyeda2kicad.py/easyeda2kicad to plugins
+git clone https://github.com/uPesy/easyeda2kicad.py
+cp -r easyeda2kicad.py/easyeda2kicad plugins/.
+zip -r Import-LIB-KiCad-Plugin.zip plugins/easyeda2kicad
+rm -rf easyeda2kicad.py
+rm -rf plugins/easyeda2kicad
