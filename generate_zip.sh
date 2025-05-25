bbdd858df56ab1b270f56ed44b784c5f738615e5
Steffen Wittemeier <uni@steffenwittemeier.de>
2025-05-25 11:46:05 +0200

    update requirements

--- generate_zip.sh
+++ generate_zip.sh
@@ -4,7 +4,7 @@ rm Import-LIB-KiCad-Plugin.zip
 mv metadata.json metadata_.json
 jq --arg today "$(date +%Y.%m.%d)" '.versions[0].version |= $today' metadata_.json > metadata.json
 
-git ls-files  -- 'metadata.json' 'resources*.png' 'plugins*.png' 'plugins*.py' | xargs zip Import-LIB-KiCad-Plugin.zip
+git ls-files  -- 'metadata.json' 'resources/*.png' 'plugins/*.png' 'plugins/*.py' 'plugins/plugin.json'  'plugins/requirements.txt' 'plugins/kicad_advanced' | xargs zip Import-LIB-KiCad-Plugin.zip
 mv metadata_.json metadata.json
 
 echo "ZIP file created: $(realpath Import-LIB-KiCad-Plugin.zip)"
